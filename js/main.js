c3ead9381f28daf6633407d4b3882d40dd059e63
bsoetarto7 <bsoetarto7@gmail.com>
2017-07-19 22:41:47 -0700

    Added select tag and functionality ajax

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-  console.log('hello');
+  $('#select-categories-dropdown').change(function() {
+    if ($(this).val()!=''){
+      var url = 'https://api.nytimes.com/svc/topstories/v2/';
+      url += $(this).val()+'.json';
+      url += '?' + $.param({
+        'api-key': "d0b63b20baa747d584dedcc2cce0acb1"
+      });
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(data) {
+        console.log(data);
+        var numberOfStories = 1;
+        $.each(data.results,function(index, items){
+          if(numberOfStories<=12){
+            if(items.multimedia.length>0){
+              console.log(items);
+              numberOfStories++;
+            }
+          }
+        });
+      }).fail(function(err) {
+        throw err;
+      });
+    }
+  });
 });

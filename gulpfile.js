9cda4408cb5054b2ccd9e444c10c01d94c677b14
bsoetarto7 <bsoetarto7@gmail.com>
2017-07-20 15:31:00 -0700

    SASS up my project

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,36 @@ var gulp          = require('gulp'),
     uglify        = require('gulp-uglify'),
     rename        = require('gulp-rename'),
     watch         = require('gulp-watch'),
-    browserSync   = require('browser-sync').create(),
-    eslint        = require('gulp-eslint');
+    eslint        = require('gulp-eslint'),
+    sass          = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    cssnano       = require('gulp-cssnano'),
+    prettyError   = require('gulp-prettyerror'),
+    browserSync   = require('browser-sync').create();
 
+// Compiling SCSS files
+gulp.task('sass', function() {
+    gulp.src('./sass/style.scss')
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./build/css'));
+});
 // Minifying javascript files
-gulp.task('scripts', function(){
-  gulp.src('./js/*.js')
-      .pipe(uglify())
-      .pipe(rename({ extname: '.min.js' }))
-      .pipe(gulp.dest('./build/js'))
+gulp.task('scripts',['lint'], function(){
+    gulp.src('./js/*.js')
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('./build/js'))
 });
 // Lint task for checking 
 gulp.task('lint', function(){
-  return gulp.src(['./js/*.js'])
+    return gulp.src(['./js/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
@@ -27,11 +44,12 @@ gulp.task('browser-sync', function() {
             baseDir: "./"
         }
     });
-    gulp.watch('build/js/*.js').on('change',browserSync.reload);
+    gulp.watch(['build/js/*.js','build/css/*.css']).on('change',browserSync.reload);
 });
-// Watch task to minify javascript
+// Watch task
 gulp.task('watch', function() {
-   gulp.watch('js/*.js', ['scripts']);
+    gulp.watch('js/*.js', ['scripts']);
+    gulp.watch('sass/*.scss', ['sass']);
 });
 
 gulp.task('default',['watch','browser-sync']);

8dd08fdff557cf84623de503e53944684cc6c0a1
bsoetarto7 <bsoetarto7@gmail.com>
2017-07-22 18:28:32 -0700

    Fix bug to remove the list item and clean up code

--- js/main.js
+++ js/main.js
@@ -8,19 +8,19 @@ $(document).ready(function(){
   })
   .change(function() { // When there is a change in the select drop down function
     if ($(this).val()!=''){ // When the selected value is not empty
-      $('.top-stories-item').remove(); // Remove existing top stories item html dom
+      $('.top-stories-list-item').remove(); // Remove existing top stories item html dom
       if(previousSelected == ''){ // If the the previous selected item was from the default
         $('.ina-header .header-logo-select-section').append('<img class="ina-loading-gif" src="img/ajax-loader.gif" alt="loading gif">'); // add loading gif beside the select drop down (desktop) and under it (mobile)
       }else{
         $('.ina-top-stories-section').prepend('<img class="ina-loading-gif" src="img/ajax-loader.gif" alt="loading gif">'); // Add the loading gif on the content section
       }
       previousSelected = $(this).val(); // update previous select value
-      $('.ia-top-stories-section').addClass('container-fluid'); // add the container class to content
+      $('.ina-top-stories-section').addClass('container-fluid'); // add the container class to content
       // Assign url into variable and add select value also with query API
       var url = 'https://api.nytimes.com/svc/topstories/v2/';
       url += $(this).val()+'.json';
       url += '?' + $.param({
-        'api-key': "d0b63b20baa747d584dedcc2cce0acb1"
+        'api-key': 'd0b63b20baa747d584dedcc2cce0acb1'
       });
       // Do a GET AJAX call
       $.ajax({
@@ -37,7 +37,7 @@ $(document).ready(function(){
           if(numberOfStories<=12){ // If the counter is still below 12, meaning if the number of stories to append has not reach below 12
             if(items.multimedia.length>0){ // Check if there is a multimedia url 
               // Store the content to display in to the topStories variable for append
-              topStories += '<li class="flex-item-mobile-100 flex-item-tablet-50 flex-item-desktop-25">';
+              topStories += '<li class="top-stories-list-item flex-item-mobile-100 flex-item-tablet-50 flex-item-desktop-25">';
               topStories += '<a href="'+items.url+'" target="_blank">';
               topStories += '<div class="top-stories-item" style="background-image:url(\''+items.multimedia[4].url+'\')">';
               topStories += '<p>'+items.abstract+'</p>';
@@ -55,7 +55,7 @@ $(document).ready(function(){
     else{ // when the selected value is empty
       $('.ina-header').removeClass('ina-header-small'); // change back to the header to default
       $('.ina-footer').removeClass('ina-footer-small'); // change back to the footer to default
-      $('.top-stories-item').remove(); // remove items in the stories section
+      $('.top-stories-list-item').remove(); // remove items in the stories section
       $('.ina-loading-gif').remove(); // remove the loading gif
       $('.ina-top-stories-section').removeClass('container-fluid'); // remove the content class
       previousSelected = $(this).val(); // update previous selected value
